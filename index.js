0f7797128d5030f6b62f1c5611114f10180047da
Phu <duongquocphu1996@gmail.com>
2021-06-17 14:27:06 +0700

    add carousel into Slider

---
+++ index.js
@@ -0,0 +1,70 @@
+/*Slider */
+var slideIndex = 1;
+
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+/*NavMobile */
+
+var header = document.getElementById("header");
+var mobileMenu = document.getElementById("mobile-menu");
+var headerHeight = header.clientHeight;
+
+// Open/Close mobile menu
+mobileMenu.onclick = function () {
+  var isClosed = header.clientHeight === headerHeight;
+  if (isClosed) {
+    header.style.height = "auto";
+  } else {
+    header.style.height = "";
+  }
+};
+
+//Auto close when choose menu
+var menuItems = document.querySelectorAll("#nav li a[href*='#']");
+
+for (var i = 0; i < menuItems.length; i++) {
+  var menuItem = menuItems[i];
+
+  menuItem.onclick = function (event) {
+    var isParentMenu =
+      this.nextElementSibling &&
+      this.nextElementSibling.classList.contains("subnav");
+    if (isParentMenu) {
+      event.preventDefault();
+    } else {
+      header.style.height = "";
+    }
+  };
+}
