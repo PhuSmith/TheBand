90df4201b4cf8689db787ea264a561c5fb3de11f
Phu <duongquocphu1996@gmail.com>
2021-08-28 22:12:27 +0700

    Add buy ticket modal

--- index.js
+++ index.js
@@ -68,3 +68,24 @@ for (var i = 0; i < menuItems.length; i++) {
     }
   };
 }
+
+const buyBtns = document.querySelectorAll(".js-buy-ticket");
+const modal = document.querySelector(".js-modal");
+const modalContainer = document.querySelector(".js-modal-container");
+const modalClose = document.querySelector(".js-modal-close");
+function showBuyTickets() {
+  modal.classList.add("open");
+}
+function hideBuyTickets() {
+  modal.classList.remove("open");
+}
+for (const buyBtn of buyBtns) {
+  buyBtn.addEventListener("click", showBuyTickets);
+}
+modalClose.addEventListener("click", hideBuyTickets);
+
+modal.addEventListener("click", hideBuyTickets);
+
+modalContainer.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
